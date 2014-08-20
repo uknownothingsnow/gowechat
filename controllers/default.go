0f9e5fbf21b65a8fcabe8ba9657722593f945f68
Bruce Lee <zhenyue.li@baidao.com>
2014-08-20 22:33:40 +0800

    use 3rd party wechat lib to handle wechat message

--- controllers/default.go
+++ controllers/default.go
@@ -1,69 +1,17 @@
 package controllers
 
 import (
-	"crypto/sha1"
-	"fmt"
 	"github.com/astaxie/beego"
-	"io"
-	"sort"
 )
-
-const (
-	TOKEN = "bruceinpeking"
-)
-
-func CheckErr(field string, err interface{}) {
-	if err != nil {
-		panic(field + " is null")
-	}
-}
-
-type Echo struct {
-	Signature string
-	Timestamp int64
-	Nonce     int64
-	EchoStr   string
-}
 
 type MainController struct {
 	beego.Controller
 }
 
-func (this *MainController) getEcho() *Echo {
-	signature := this.GetString("signature")
-	timestamp, err := this.GetInt("timestamp")
-	CheckErr("timestamp", err)
-	nonce, err := this.GetInt("nonce")
-	CheckErr("nonce", err)
-	echoStr := this.GetString("echoStr")
-	return &Echo{
-		signature,
-		timestamp,
-		nonce,
-		echoStr,
-	}
-}
-
-func checkSignature(echo *Echo) bool {
-	tmps := []string{TOKEN, string(echo.Timestamp), string(echo.Nonce)}
-	sort.Strings(tmps)
-	tmpStr := tmps[0] + tmps[1] + tmps[2]
-	tmp := str2sha1(tmpStr)
-	return tmp == echo.Signature
-}
-
-func str2sha1(data string) string {
-	t := sha1.New()
-	io.WriteString(t, data)
-	return fmt.Sprintf("%x", t.Sum(nil))
-}
-
 func (this *MainController) Get() {
-	echo := this.getEcho()
-	ret := checkSignature(echo)
-	if ret {
-		this.Ctx.WriteString(echo.EchoStr)
-	} else {
-		this.Ctx.WriteString("Error, echoStr not match!")
-	}
+
+}
+
+func (this *MainController) Post() {
+
 }
--- main.go
+++ main.go
@@ -1,11 +1,38 @@
 package main
 
 import (
-	_ "wechat/routers"
 	"github.com/astaxie/beego"
+	"github.com/chanxuehong/wechat/message/request"
+	"github.com/chanxuehong/wechat/message/response"
+	"github.com/chanxuehong/wechat/server"
+	"net/http"
 )
 
+const (
+	TOKEN = "your_weixin_token"
+)
+
+// 一般一个应用维护一个实例即可
+var wechatHandler *server.Handler
+
 func main() {
+	// 初始化 wechatHandler
+	setting := &server.HandlerSetting{
+		Token:              TOKEN,
+		TextRequestHandler: TextRequestHandler,
+	}
+	wechatHandler = server.NewHandler(setting) // 并发安全，一般一个应用只用一个实例即可
+
+	beego.Handler("/wechat", wechatHandler)
 	beego.Run()
 }
 
+// 自定义文本消息处理函数
+func TextRequestHandler(w http.ResponseWriter, r *http.Request, text *request.Text) {
+	//TODO: 添加你的代码，下面只是示例代码！
+	// 把用户发送过来的文本原样的回复过去
+	resp := response.NewText(text.FromUserName, text.ToUserName, text.Content)
+
+	w.Header().Set("Content-Type", "application/xml; charset=utf-8") // 可选
+	server.WriteText(w, resp)
+}
